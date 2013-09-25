7c57882e9a0944bbd8edc5ffb5951f56f8f5ae04
David Gay <oddshocks@riseup.net>
2013-09-25 15:43:58 -0400

    Remove old enable and disable shell scripts (DEPRECATED).

--- disable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This overwrites yumwat's config file so that yumwat is disabled.
-# I wrote it to easy enable and disable yumwat during development.
-# Requires root privs.
-
-# Rewrite config so plugin is enabled
-printf "[main]\nenabled=0\ntimid=0" > /etc/yum/pluginconf.d/yumwat.conf
