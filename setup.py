e8771e806b0c83964c98120283b340755410ecb6
David Gay <oddshocks@riseup.net>
2013-09-25 15:04:07 -0400

    Run yumwat on the project.

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+version = "0.3.0"
+
+setup(
+    name="yumwat",
+    version=version,
+    description="Prints package descriptions. A yum plugin for when you wat.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords="yum plugin wat yumwat package descriptions",
+    author="David Gay",
+    author_email="oddshocks@riseup.net",
+    url="https://github.com/oddshocks/yumwat",
+    license="GPL v3+",
+    packages=find_packages(
+    ),
+    scripts=[
+        "distribute_setup.py",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "",
+    ],
+    #TODO: Deal with entry_points
+    #entry_points="""
+    #[console_scripts]
+    #pythong = pythong.util:parse_args
+    #"""
+)
